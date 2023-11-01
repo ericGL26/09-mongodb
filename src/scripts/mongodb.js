51a090647b1a3788976740881ae578de36a6e3f8
Eduardo3007 <eduardo.oliveira@elastic.fit>
2023-11-01 18:02:27 -0300

    first commit

---
+++ src/scripts/mongodb.js
@@ -0,0 +1,5 @@
+db.herois.insert({
+    nome: 'Flash',
+    poder: 'Velocidade',
+    dataNascimento: '1940-03-04'
+})
--- src/testes/postgresStrategies.js
+++ src/testes/postgresStrategies.js
@@ -3,7 +3,7 @@ const Postgres = require('../../db/strategies/postgres');
 const Context = require('../../db/strategies/base/contextStrategy');
 
 const context = new Context(new Postgres());
-const MOCK_HEROI_CADASTRAR = { nome: 'Gaviao Negro', poder: 'Flechas' };
+const MOCK_HEROI_CADASTRAR = { nome: 'Batman', poder: 'Predador' };
 const MOCK_HEROI_ATUALIZAR = { nome: 'Batman', poder: 'Predador' };
 
 describe('Postgres Strategy', function () {
@@ -20,17 +20,20 @@ describe('Postgres Strategy', function () {
         assert.equal(result, true);
     });
 
-    it('cadastrar', async function () {
-        const result = await context.create(MOCK_HEROI_CADASTRAR);
-        delete result.id;
-        assert.deepEqual(result, MOCK_HEROI_CADASTRAR);
-    });
+    //it('cadastrar', async function () {
+        //const result = await context.create(MOCK_HEROI_CADASTRAR);
+        //delete result.id;
+        //assert.deepEqual(result, MOCK_HEROI_CADASTRAR);
+    //});
 
-    it('listar', async function () {
-        const [result] = await context.read({ nome: MOCK_HEROI_CADASTRAR.nome });
-        delete result.id;
-        assert.deepEqual(result, MOCK_HEROI_CADASTRAR);
+    it.only('listar', async function () {
+        const result = await context.read();
+        console.log('Valores do banco de dados:', result);
+        // Substitua MOCK_HEROI_CADASTRAR pelo valor esperado
+        //const expectedValues = []; // Preencha com os valores esperados do banco de dados
+        //assert.deepEqual(result, expectedValues);
     });
+    
 
     it('Remover por id', async function () {
         const [item] = await context.read({});
@@ -38,17 +41,20 @@ describe('Postgres Strategy', function () {
         assert.deepEqual(result, 1);
     });
 
-    it.only('atualizar', async function () {
+    it('atualizar', async function () {
         const [itemAtualizar] = await context.read({ nome: MOCK_HEROI_ATUALIZAR.nome });
         const novoItem = {
-            nome: 'Mulher Maravilha', // Não é necessário copiar todo o MOCK_HEROI_ATUALIZAR
-            poder: 'Super Força'     // Apenas atualize os campos necessários
+            nome: 'Batman atualizado',
+            poder: 'Iii'
         };
         const result = await context.update(itemAtualizar.id, novoItem);
-        assert.deepEqual(result, 1);
+        console.log('RESULTADO', result)
+        console.log([itemAtualizar])
+        //assert.deepEqual(result, 1);
 
         // Verifique se o item foi atualizado corretamente
         const [itemAtualizado] = await context.read({ id: itemAtualizar.id });
+        console.log('AAAaaaaAAAA', itemAtualizado);
         assert.equal(itemAtualizado.nome, novoItem.nome);
         assert.equal(itemAtualizado.poder, novoItem.poder);
     });
